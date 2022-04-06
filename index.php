5f796aa608be2457a983485cbe187a8b95489f44
Pp <jmartinezi@esliceu.net>
2022-04-06 09:33:09 +0200

    es meu commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PP'S PAGE</title>
+    <meta charset="utf-8" />
+    <title>INDEX</title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+</head>
+
+<body>
+    <a href="insert.html">REGISTRARTE</a>
+    <a href="pagina2.html">PLACA BASE</a>
+</body>
+
+</html>
---
+++ pagina2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PP'S PAGE</title>
+    <meta charset="utf-8" />
+    <title>PP'S PAGE</title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <link href="css/style.css" />
+</head>
+
+<body>
+    <a href="index.html">INDEX</a>
+    <a href="insert.html">REGISTRARTE</a>
+    <h1>PLACA BASE</h1>
+    <p>La placa mare, placa base o targeta mare (en anglès motherboard)[1] és la targeta de circuits impresos central a
+        l'ordinador que conté el microprocessador, la memòria RAM del sistema, circuits electrònics de suport, la ROM i
+        ranures especials (slots) que permeten la connexió de targetes adaptadores addicionals. Aquestes targetes solen
+        realitzar funcions de control de perifèrics, com monitors, impressores, unitats de disc, etc. </p>
+</body>
+
+</html>
