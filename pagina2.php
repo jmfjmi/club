6000af00fe9e11c9c9f8c5a0ee9085007ed6c337
Jandreum12 <Jandreum@esliceu.net>
2022-05-10 10:21:16 +0200

    canvi a pagina 2

--- pagina2.php
+++ pagina2.php
@@ -7,7 +7,7 @@ include 'includes/header.php';
 ?>
 <body>
     <h1>PLACA BASE</h1>
-    <p>La placa mare, placa base o targeta mare (en anglès motherboard)[1] és la targeta de circuits impresos central a
+    <p>La placa mare, placa base o targeta mare (en anglès motherboard) és la targeta de circuits impresos central a
         l'ordinador que conté el microprocessador, la memòria RAM del sistema, circuits electrònics de suport, la ROM i
         ranures especials (slots) que permeten la connexió de targetes adaptadores addicionals. Aquestes targetes solen
         realitzar funcions de control de perifèrics, com monitors, impressores, unitats de disc, etc. </p>
