53e212c1f490062e13b8afdfb33cf23c7b7ec55c
Pp <jmartinezi@esliceu.net>
2022-05-17 11:19:46 +0200

    insert

--- scripts/insert_soci.php
+++ scripts/insert_soci.php
@@ -2,9 +2,21 @@
 
 include '../includes/database.php';
 
-$nom = $_POST['nomSoci'];
+$nom = $_POST['Nom'];
+$DNI = $_POST['DNI'];
+$p_cognom = $_POST['pCognom'];
+$s_cognom = $_POST['sCognom'];
+$datadenaixament = $_POST['DataDeNaixament'];
+$email = $_POST['email'];
+$telefon = $_POST['telefon'];
 
 
-$query = "INSERT INTO Socis (Nom, 1rCognom, 2nCognom, DataDeNaixament, email, telefon ) VALUES ('$Nom', '$1rCognom', '$2nCognom', '$DataDeNaixament', '$email', '$telefon')";
+$query = "INSERT INTO Socis (Nom, DNI, 1rCognom, 2nCognom, DataDeNaixament, email, telefon ) VALUES ('$nom', '$DNI', '$p_cognom', '$s_cognom', '$datadenaixament', '$email', '$telefon')";
 
-mysqli_query($dbh, $query);
+$result = mysqli_query($dbh, $query);
+
+if($result){
+    header('Location: ../socis.php');
+}else{
+    echo mysqli_error($dbh);
+}
