e8e698c77c7a061234db5a3d42dc4bc72c434a30
Alex <ARosal708@gmail.com>
2022-06-07 11:55:31 +0200

    update

--- insert2_equip.php
+++ insert2_equip.php
@@ -32,13 +32,26 @@ include 'includes/header.php';
 	<label for="nombre">Nom</label>
 	<input type="text" name="Nom" value="<?=$equip['Nom']?>" id="Nom"  placeholder="Escribe el nombre del equipo"/>
 
-    <label for="nombre">fkidClub</label>
-	<input type="text" name="fkidClub" value="<?=$equip['fkidClub']?>"id="fkidClub" placeholder="Club"/>
+    <select name="fkidCategoria">
+		<?php
+		$query = "SELECT * FROM Categoria";
+		$result = mysqli_query($dbh, $query) or die(mysqli_error($dbh));
+		while($equip = mysqli_fetch_assoc($result)){
+			echo '<option value="' . $Categoria['idCategoria'] . '">'. $Categoria['nom'] . '</option>';
+		}
+		?>
+	</select>
 
-	<label for="apellidos">Segon llinatge</label>
-	<input type="text" name="fkidCategoria"value="<?=$equip['fkidCategoria']?>" id="fkidCategoria" placeholder="Categoria"/>
+	<select name="fkidClub">
+		<?php
+		$query = "SELECT * FROM Club";
+		$result = mysqli_query($dbh, $query) or die(mysqli_error($dbh));
+		while($equip = mysqli_fetch_assoc($result)){
+			echo '<option value="' . $Club['idClub'] . '">'. $Club['nom'] . '</option>';
+		}
+		?>
+	</select>
 
-	
 	<input type="submit" name="enviar" value="ENVIAR"/>
 </form>
 
